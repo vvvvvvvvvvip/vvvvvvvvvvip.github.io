3800060f8bda117270da7c0164e0d18107e3f9d1
vvvvvvvvvvip <18334795012@163.com>
2018-07-02 16:39:07 +0800

    主页修改

---
+++ js/index.js
@@ -0,0 +1,7 @@
+ // var swiper = new Swiper('.swiper-container', {
+ // 	pagination: '.swiper-pagination',
+ // 	paginationClickable: true,
+ // 	paginationBulletRender: function (index, className) {
+ // 		return '<span class="' + className + '">' + (index + 1) + '</span>';
+ // 	}
+ // });
